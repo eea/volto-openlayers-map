749d3c3a90ca196386346a0fe8bcc27cab77f262
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2023-02-15 22:02:34 +0200

    Add useMapContext

--- src/hocs/withMapContext.js
+++ src/hocs/withMapContext.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react';
 import { MapContext } from '../Map';
 
+export const useMapContext = () => useContext(MapContext);
+
 const withMapContext = (WrappedComponent) =>
   React.forwardRef((props, ref) => {
     const context = useContext(MapContext);
