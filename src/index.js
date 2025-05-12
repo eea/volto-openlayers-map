6a0828e7331bb1cdd916373e7fdb26cb398a3673
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2025-05-12 13:12:33 +0300

    Provide withOpenLayers

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import loadable from '@loadable/component';
+import { useLazyLibs } from '@plone/volto/helpers/Loadable/Loadable';
 
 const applyConfig = (config) => {
   config.settings.loadables = {
@@ -17,14 +18,61 @@ const applyConfig = (config) => {
     olSource: loadable.lib(() => import('ol/source')),
     olStyle: loadable.lib(() => import('ol/style')),
     olTilegrid: loadable.lib(() => import('ol/tilegrid')),
-    olOverlay: loadable.lib(() => import('ol/Overlay').default),
+    olOverlay: loadable.lib(() => import('ol/Overlay')),
     olEvents: loadable.lib(() => import('ol/events')),
     olCondition: loadable.lib(() => import('ol/events/condition')),
     olRender: loadable.lib(() => import('ol/render')),
-
-    // olMapboxStyle: loadable.lib(() => import('ol-mapbox-style')),
   };
   return config;
 };
+
+const libraries = [
+  'ol',
+  'olCondition',
+  'olControl',
+  'olCoordinate',
+  'olEvents',
+  'olExtent',
+  'olFormat',
+  'olGeom',
+  'olInteraction',
+  'olLayer',
+  'olLoadingstrategy',
+  'olOverlay',
+  'olProj',
+  'olRender',
+  'olSource',
+  'olStyle',
+  'olTilegrid',
+];
+
+export function withOpenLayers(WrappedComponent) {
+  function WrapperComponent(props) {
+    const loaded = useLazyLibs(libraries, { shouldRerender: true });
+    const ol = {
+      ol: loaded.ol,
+      condition: loaded.olCondition,
+      control: loaded.olControl,
+      coordinate: loaded.olCoordinate,
+      events: loaded.olEvents,
+      extent: loaded.olExtent,
+      format: loaded.olFormat,
+      geom: loaded.olGeom,
+      interaction: loaded.olInteraction,
+      layer: loaded.olLayer,
+      loadingstrategy: loaded.olLoadingstrategy,
+      Overlay: loaded.olOverlay?.default,
+      proj: loaded.olProj,
+      render: loaded.olRender,
+      source: loaded.olSource,
+      style: loaded.olStyle,
+      tilegrid: loaded.olTilegrid,
+    };
+    const isLoaded = Object.keys(loaded).length === libraries.length;
+    return isLoaded ? <WrappedComponent {...props} ol={ol} /> : null;
+  }
+
+  return WrapperComponent;
+}
 
 export default applyConfig;
